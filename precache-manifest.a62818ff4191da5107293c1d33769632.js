5efc0489e26d95f7f6b46edcbc347893e5b4d87a
abuduru <abdulg1995@gmail.com>
2019-01-16 04:01:30 -0500

    Updates

---
+++ precache-manifest.a62818ff4191da5107293c1d33769632.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48f5ebd23189f40a1420",
+    "url": "/shopify-web-engineer-challenge/static/js/runtime~main.48f5ebd2.js"
+  },
+  {
+    "revision": "1edcd2b8f9373949ea55",
+    "url": "/shopify-web-engineer-challenge/static/js/main.1edcd2b8.chunk.js"
+  },
+  {
+    "revision": "5ed85477b954c23655e4",
+    "url": "/shopify-web-engineer-challenge/static/js/1.5ed85477.chunk.js"
+  },
+  {
+    "revision": "1edcd2b8f9373949ea55",
+    "url": "/shopify-web-engineer-challenge/static/css/main.2cf5ae53.chunk.css"
+  },
+  {
+    "revision": "9c885989d1c1c5515934612868e125b4",
+    "url": "/shopify-web-engineer-challenge/index.html"
+  }
+];
